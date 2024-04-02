c6702339bb4ade182960657b8171f68dde939081
Abreham Aschalew <107627151+Abresh27@users.noreply.github.com>
2024-04-02 21:17:41 +0300

    Delete Back-end directory

--- Back-end/routes/createtable.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { createTable } = require("../controller/createTable");
-//Route to create the tables in the database
-router.get("/createtable", createTable);
-
-module.exports = router;
